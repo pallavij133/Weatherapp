37bad9c720e295a7ed6edbabd7c838e9ae835847
pallavij133 <144209734+pallavij133@users.noreply.github.com>
2024-05-09 21:41:21 +0530

    weather app code

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { View } from 'react-native';
+import WeatherScreen from './Src/Screens/WeatherScreen';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <WeatherScreen />
+    </View>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+import {name as appName} from './app.json';
+import App from './App';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent(appName, () => App);
